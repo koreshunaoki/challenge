57603be6e65317ef394d5a92ca14236ec20e517c
koreshunaoki <ks.i.ey.bkkyd@gmail.com>
2022-09-30 15:58:48 +0000

    作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'blogs/edit'
 
   get 'top' => 'homes#top'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

fbdec015949d2fa9351757b3f2bd0280d0305a40
koreshunaoki <ks.i.ey.bkkyd@gmail.com>
2022-10-01 07:31:41 +0000

    作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'blogs/show'
   get 'blogs/new'
   get 'blogs/edit'
-
+  get 'homes/index'
   get 'top' => 'homes#top'
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
